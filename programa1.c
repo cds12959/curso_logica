acd149237753f89519a02e79c2fa8897898dc17b
cds12959 <91380027+cds12959@users.noreply.github.com>
2021-09-25 16:14:07 -0300

    Aula 1

---
+++ programa1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello\nworld");
+	getchar();
+
+	return 0;
+}
---
+++ programa2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Carlos Rafael\nde Oliveira Carneiro");
+	return 0;
+}
